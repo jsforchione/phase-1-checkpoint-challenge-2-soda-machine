82c95dec91eb8874c6fdc27f6fe06e4194e489a8
Jessica Forchione <jsforchione@gmail.com>
2016-08-29 15:13:07 -0700

    checkpoint challenge 2

--- soda_machine.rb
+++ soda_machine.rb
@@ -7,12 +7,21 @@ class SodaMachine
   end
 
   def current_inventory_count
+    @sodas.count
   end
 
   def find_soda(soda_brand)
+    @sodas.detect { |soda| soda.brand == soda_brand }
   end
 
   def sell(soda_brand)
+    sold_soda = find_soda(soda_brand)
+
+    return nil if sold_soda == nil 
+
+    @cash += sold_soda.price 
+    @sodas.delete(sold_soda)
+
   end
 
 end
