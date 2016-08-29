28c394da9f6b85ad916e226d041b08054db458f6
Jessica Forchione <jsforchione@gmail.com>
2016-08-29 16:55:30 -0700

    update notes for checkpoint challenge 2

--- soda_machine.rb
+++ soda_machine.rb
@@ -8,16 +8,28 @@ class SodaMachine
 
   def current_inventory_count
     @sodas.count
+    #or @sodas.length
   end
 
   def find_soda(soda_brand)
     @sodas.detect { |soda| soda.brand == soda_brand }
+    # can also use .find for above solution 
+
+    # @sodas.each do |soda|
+    #   if soda.brand == soda_brand 
+    #     soda
+    #   end 
+    # end 
+    # nil 
   end
 
   def sell(soda_brand)
     sold_soda = find_soda(soda_brand)
 
     return nil if sold_soda == nil 
+    # return nil if !sold_soda 
+    # !sold_soda is a falsey value 
+    # above is better (shorter) solution
 
     @cash += sold_soda.price 
     @sodas.delete(sold_soda)
